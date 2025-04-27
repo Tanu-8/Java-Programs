e08a38e95f8eea20f65d41e1107a0decafe6d7d0
Tanu Priya <tanu.sinha94300@gmail.com>
2025-04-27 23:10:49 +0530

    Add files via upload

---
+++ JAVA_Classroom_Solution/LargestTernary.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LargestTernary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, max;
+
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        b = sc.nextInt();
+
+        max = (a > b) ? a : b;
+
+        System.out.println("Largest number is: " + max);
+
+        sc.close();
+    }
+}
+
